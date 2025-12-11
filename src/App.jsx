381c02f5c109684fdf5bc9c55d4bf71d4a18b71d
dimas-40 <thomas.tj.park@gmail.com>
2025-12-12 00:50:37 +0900

    Fix: BrowserRouter basename for custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import About from './pages/About';
 
 function App() {
   return (
-    <BrowserRouter basename="/dtslib.com">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
